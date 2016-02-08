b8d01b0003f66e2e31e0fa6a1f74d9629cb277ab
Arik Ostler <arikostler@gmail.com>
2016-02-08 20:44:21 +0000

    Added LSB tags to startup script to get rid of errors

--- bin/runUserStartupScripts.sh
+++ bin/runUserStartupScripts.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+### BEGIN INIT INFO
+
+# Provides:				runUserStartupScripts
+# Required-Start:		$local_fs $network $remote_fs
+# Required-Stop:		$local_fs $network $remote_fs
+# Default-Start:		2 3 4 5
+# Default-Stop:			0 1 6
+# Short-Description:	Run all scripts in startup folder
+# Description:			Run all scripts in startup folder
+
+### END INIT INFO
+
+
 echo "Running startup scripts in folder: '/home/pi/.startup'"
 STARTUPDIR=/home/pi/.startup
 for i in $( ls $STARTUPDIR ); do
