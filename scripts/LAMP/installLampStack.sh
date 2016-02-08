18627ff1ff67a5857aa8b44ca296973af3f60ae1
Arik Ostler <arikostler@gmail.com>
2016-02-08 17:19:40 -0500

    Added samba script readme

--- scripts/LAMP/installLampStack.sh
+++ scripts/LAMP/installLampStack.sh
@@ -8,3 +8,6 @@ sudo apt-get install apache2 -y
 sudo apt-get install php5 libapache2-mod-php5 -y
 
 sudo apt-get install mysql-server php5-mysql -y
+
+sudo chown -R pi /var/www
+sudo chgrp -R pi /var/www
