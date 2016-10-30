7733e72b703103cbb4db6cf646cb216458f0424b
Arik Ostler <arikostler@gmail.com>
2016-10-30 12:20:48 +0000

    Updated samba script to include pishare

--- scripts/samba/sambaSetup.sh
+++ scripts/samba/sambaSetup.sh
@@ -6,7 +6,7 @@ sudo apt-get install samba -y
 
 sudo cp /etc/samba/smb.conf /etc/samba/smb.conf.backup
 
-sudo bash -c 'cat sambaShareConfigs/webshare >> /etc/samba/smb.conf'
+sudo bash -c 'cat sambaShareConfigs/pishare >> /etc/samba/smb.conf'
 
 sudo smbpasswd -a pi
 
